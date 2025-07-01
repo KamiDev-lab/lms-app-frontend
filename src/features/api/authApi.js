64861b3f7e1172966b41031b2cf37291884f6e67
Syed Kamran <kami.webdev1@gmail.com>
2025-07-01 14:27:05 +0500

    deployement live url updated.

--- src/features/api/authApi.js
+++ src/features/api/authApi.js
@@ -1,7 +1,7 @@
 import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react";
 import { userLoggedIn, userLoggedOut } from "../authSlice";
 
-const USER_API = "http://localhost:8080/api/v1/user/"
+const USER_API = "https://0f091c7e-1724-4e8e-8136-b4debbe3f317-00-2p43f0lqipxcg.sisko.replit.dev/api/v1/user/"
 
 export const authApi = createApi({
     reducerPath:"authApi",
